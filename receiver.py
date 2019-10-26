16e6bcf76b71d4d091af957296799309a640ea72
unknown <870096160@qq.com>
2019-10-26 17:26:52 -0400

    Sequence number problem left only. (95%)

--- receiver.py
+++ receiver.py
@@ -10,17 +10,28 @@ class receiver:
         #  Check if a received packet has been corrupted during transmission.
         # Return true if computed checksum is different than packet checksum.
         # 3 points
-        return
+        sum = packet.seqNum + packet.ackNum
+        for c in packet.payload:
+            sum += ord(c)
+
+        if sum == packet.checksum:
+            return False
+
+        return True
 
     def isDuplicate(self, packet):
         # check if packet sequence number is the same as expected sequence number
         # 2 points
-        return
+        if packet.seqNum == self.expectedSeqNum:
+            return False
+        return True
 
     def getNextExpectedSeqNum(self):
         # Use modulo-2 arithmetic to ensure sequence number is 0 or 1.
         # 2 points
-        return
+        self.expectedSeqNum += 1
+        self.expectedSeqNum %= 2
+        return self.expectedSeqNum
 
     def __init__(self, entityName, ns):
         self.entity = entityName
@@ -30,6 +41,7 @@ class receiver:
     def init(self):
         # initialise expected packet sequence number
         # 2 points
+        self.expectedSeqNum = 0
         return
 
     def input(self, packet):
@@ -37,10 +49,21 @@ class receiver:
         # arrives at the receiver.
 
         # If packet is corrupted or duplicate: send ACK with wrong sequence number.
+        if self.isCorrupted(packet) or self.isDuplicate(packet):
+            packet.payload = ''
+            packet.ackNum += 1
+            packet.ackNum %= 2
+            packet.checksum = packet.seqNum + packet.ackNum
+            self.networkSimulator.udtSend(B, packet)
+            return
+
         # If packet is OK, deliver it and send correct ACK.
-        packet.payload = ''
-        self.networkSimulator.udtSend(B, packet)
-        self.expectedSeqNum = 1
         # update expected sequence number
         # 12 points
+        self.networkSimulator.deliverData(B, packet.payload)
+        packet.payload = ''
+        packet.checksum = packet.seqNum + packet.ackNum
+        self.networkSimulator.udtSend(B, packet)
+        self.expectedSeqNum = self.getNextExpectedSeqNum()
+
         return
--- sender.py
+++ sender.py
@@ -12,19 +12,27 @@ class sender:
         #  Check if a received packet (ACK) has been corrupted during transmission.
         # similar to the corresponding function in receiver side
         # 3 points
-        return
+        sum = packet.seqNum + packet.ackNum
+        if packet.checksum == sum:
+            return False
+        return True
 
     def isDuplicate(self, packet):
         # checks if an ACK is duplicate or not
         # similar to the corresponding function in receiver side
         # 2 points
-        return
+        if packet.ackNum == self.currentSeqNum:
+            return False
+        return True
 
     def getNextSeqNum(self):
         # generate the next sequence number to be used
         # similar to the corresponding function in receiver side
         # 2 points
-        return
+        self.currentSeqNum += 1
+        self.currentSeqNum %= 2
+        self.ACK = self.currentSeqNum
+        return self.currentSeqNum
 
     def __init__(self, entityName, ns):
         self.entity = entityName
@@ -34,6 +42,8 @@ class sender:
     def init(self):
         # initialize the currentSeqNum  and currentPacket
         # 4 points
+        self.currentSeqNum = 0
+        self.currentPacket = None
         return
 
     def timerInterrupt(self):
@@ -41,17 +51,21 @@ class sender:
         # It sends the packet again.
         # It starts the timer, sets the timeout value to be twice the RTT
         # 6 points
+        self.networkSimulator.udtSend(A, self.currentPacket)
+        self.networkSimulator.startTimer(A, 2 * self.RTT)
         return
 
     def output(self, message):
         # if current packet is not transmitted ignore the message
-
         # else prepare a packet and send it through the network layer
         # call utdSend
         # start the timer
         # 12 points
-        checksum = 1
-        self.currentPacket = Packet(self.currentSeqNum, self.currentSeqNum, checksum, message.data)
+        checksum = self.currentSeqNum + self.ACK
+        for m in message.data:
+            checksum += ord(m)
+
+        self.currentPacket = Packet(self.currentSeqNum, self.ACK, checksum, message.data)
         self.networkSimulator.udtSend(A, self.currentPacket)
         self.networkSimulator.startTimer(A, self.RTT)
         return
@@ -60,9 +74,13 @@ class sender:
         # If ACK isn't corrupted or duplicate, transmission complete.
         # so the currentPacket should be set to None
         # You do not need to do anything else.
-        self.networkSimulator.stopTimer(A)
-        self.currentPacket = None
+        if not self.isDuplicate(packet) and not self.isCorrupted(packet):
+            self.networkSimulator.stopTimer(A)
+            self.currentPacket = None
+            self.currentSeqNum = self.getNextSeqNum()
+            return
         # In the case of duplicate ACK the packet will be sent again.
         # since you do not change the current packet in that case
         # 8 points
+        self.networkSimulator.udtSend(A, packet)
         return
